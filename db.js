55e350d4f291886c095049e13f1ab13f3b5a2d3c
Dimitar Ivanov <dimitarivanov2525@gmail.com>
2025-01-08 09:30:37 +0200

    add db.js

---
+++ db.js
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv';
+dotenv.config(); // Load .env variables
+
+import pg from 'pg';
+
+const { Pool } = pg;
+
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD ? "Loaded" : "Not Loaded");
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: String(process.env.DB_PASSWORD),
+  port: process.env.DB_PORT
+});
+
+pool.connect()
+  .then(() => console.log('✅ Connected to PostgreSQL'))
+  .catch(err => console.error('❌ Connection error', err));
+
+export default pool;
