20c126aa8bca5405ab876cdf65923f91e0f155e9
Dimitar Ivanov <dimitarivanov2525@gmail.com>
2025-01-08 09:41:04 +0200

    fix

--- db.js
+++ db.js
@@ -13,7 +13,10 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: String(process.env.DB_PASSWORD),
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false, // Allow self-signed certificates
+  }
 });
 
 pool.connect()
